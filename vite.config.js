c7cd7795a03a21614db75030eaa986dd60518755
Jasper de Langen <studiojasperdelangen@proton.me>
2026-07-31 15:20:53 +0200

    Build crawlable pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './'
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'index.html'),
+        over: resolve(__dirname, 'over/index.html'),
+        panoptica: resolve(__dirname, 'panoptica/index.html'),
+        proeftuin: resolve(__dirname, 'proeftuin/index.html'),
+        experimenten: resolve(__dirname, 'experimenten/index.html')
+      }
+    }
+  }
 })
